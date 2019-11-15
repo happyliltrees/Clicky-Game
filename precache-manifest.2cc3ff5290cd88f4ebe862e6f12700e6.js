01b7c94a124afa6b3912266760e27bf7b858846d
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-11-14 23:02:01 -0500

    Updates

---
+++ precache-manifest.2cc3ff5290cd88f4ebe862e6f12700e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "055d8d2f978a61636f08",
+    "url": "/Clicky-Game/static/js/main.d81d1a97.chunk.js"
+  },
+  {
+    "revision": "49db0d9fc976f003b3ff",
+    "url": "/Clicky-Game/static/js/2.458af4f8.chunk.js"
+  },
+  {
+    "revision": "055d8d2f978a61636f08",
+    "url": "/Clicky-Game/static/css/main.18dd23fa.chunk.css"
+  },
+  {
+    "revision": "cf8168bdeb2c05af22993bff7cb06deb",
+    "url": "/Clicky-Game/index.html"
+  }
+];
